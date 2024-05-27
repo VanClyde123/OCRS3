cf83706b9df8de10206782e75e429a847b4ef20b
Van <vanmenis123@gmail.com>
2024-05-27 13:07:23 +0800

    The Update list are in the README.MD

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -9,7 +9,7 @@ class SectionController extends Controller
 {
    public function getSections($subjectId)
 {
-    // Fetch sections associated with the subject ID
+   
     $sections = Section::where('subject_description_id', $subjectId)->get();
 
     return response()->json(['sections' => $sections]);
